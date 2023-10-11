6769be6b911c5d1aed8b07e0fb37eb36e20d75e2
Maximiliano R Soria <MaximilianoRodrigoSoria@gmail.com>
2023-10-11 11:34:19 -0300

    Creacion de Product, User, UserType

---
+++ src/main/java/ar/com/laboratory/ecommerce/domain/User.java
@@ -0,0 +1,24 @@
+package ar.com.laboratory.ecommerce.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer id;
+    private String username;
+    private String fistName;
+    private String lastName;
+    private String email;
+    private String address;
+    private String cellphone;
+    private String password;
+    private UserType userType;
+    private LocalDateTime dateCreated;
+}
